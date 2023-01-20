fbfee274362dbdca48f04eaeb44ef6a8374bb54c
Adilnurlan <nurlan.adil98@gmail.com>
2023-01-20 19:39:37 +0600

    fourth

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import s from './ContactForm.module.css';
+import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContacts } from 'redux/contacts/contacts-operations';
+import { selectContacts } from 'redux/selectors';
+
+export const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const contacts = useSelector(selectContacts);
+  const dispatch = useDispatch();
+
+  const handleChange = ({ currentTarget: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+    }
+  };
+
+  const formSubmit = newContact => {
+    const isExist = contacts.find(
+      cont =>
+        cont.name.toLowerCase().trim() === newContact.name.toLowerCase().trim()
+    );
+    if (isExist) {
+      return alert(`${newContact.name} is already in contacts`);
+    } else if (
+      (newContact.name.trim() === '', newContact.number.trim() === '')
+    ) {
+      return alert('Заполните все поля');
+    }
+    dispatch(addContacts(newContact));
+  };
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    formSubmit({ name, number, id: nanoid() });
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form className={s.form} onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label>
+        Number
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button>Add contact</button>
+    </form>
+  );
+};
